9532432a1d5faec6bc9f1c7051075cff336460f9
Ronaldocter <ronaldfernandez3000@gmail.com>
2022-03-08 14:01:19 -0500

    Inicio mi proyecto real

---
+++ Ejercicios.py
@@ -0,0 +1,81 @@
+from ast import Str
+
+
+a = '10'
+
+a1 = "5"
+
+a2 = "ronald"
+
+print("hola tengo {} quiero {} manzanas me llamo {}" . format(a,a1,a2))           
+
+n = "r"
+
+n = 10.2
+
+print(float(n))
+
+valor = int(input("ingrese numero:"))
+
+print(valor)
+
+if (valor)>7:
+
+    print("si es mayor ")
+  
+else:
+    print("no es mayor ")
+
+
+
+#Ejercicios     
+
+a = 1
+a = "hola"
+a = 'hola'
+print(a)
+
+a = 1
+b = 1
+print(a+b)
+
+a = 1
+a = '10'
+a1 = "5"
+a2 = "jose"
+
+print('tengo {} y quiero {} mansanaz me llamo {}'.format(a,a1,a2))
+
+a = 'a'
+a = 10.2
+
+print(a)
+print(int(a))
+
+x = True
+X = False
+
+valor = int(input('digite su numero:'))
+dato = str(input('ingrese su nombre:'))
+
+print(valor,dato)
+
+#Ejercicio de listas
+
+lista=[]
+
+lista.append(9)
+
+print(lista)
+
+lista=[1234567]
+
+lista.append(0)
+
+print(lista)
+
+listaA=[1,2,3,3,5,4,6,6,8,7]
+
+listaA.sort()
+
+print(listaA)
